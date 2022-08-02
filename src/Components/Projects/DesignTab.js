bca47fabd5584b124e1139afdf4c165b04ab9e1e
leoarthurwall <leowall93@gmail.com>
2022-08-02 23:23:45 +0100

    project design tab styled. added action logos. mapped through tech array. Added a · between words, removed last child. Added stock image to see how it looks

--- src/Components/Projects/DesignTab.js
+++ src/Components/Projects/DesignTab.js
@@ -1,18 +1,22 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+
 const DesignTab = () => {
   const design = [
     {
-      image: "design image 1",
-      title: "design title 1",
-      summary: "A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track.",
-      technologies: ["HTML  ", "CSS  ", "React  ", "JS  "],
-      links: ["visit ", " home"],
+      image: "https://www.infobae.com/new-resizer/gvU1Me_FuUIB9sryOC9LlGtTHmc=/768x512/filters:format(webp):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/5TD7NOGP5NECNAZ3L6CG7TPIK4.jpg",
+      title: "The Spotify Project",
+      summary:
+        "A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information.",
+      technologies: ["HTML", "CSS", "React", "JS", "Next"],
+      links: ["visit", " home"],
       id: 1,
     },
     {
       image: "design image 2",
       title: "design title 2",
       summary: "design summary 2",
-      technologies: ["HTML  ", "CSS  ", "React  ", "JS  "],
+      technologies: ["HTML", "CSS", "React", "JS"],
       links: ["visit ", "home"],
       id: 2,
     },
@@ -20,30 +24,43 @@ const DesignTab = () => {
       image: "design image 3",
       title: "design title 3",
       summary: "design summary 3",
-      technologies: ["HTML  ", "CSS  ", "React  ", "JS  "],
+      technologies: ["HTML", "CSS", "React", "JS"],
       links: ["visit ", "home"],
       id: 3,
     },
   ];
-
+  console.log(design[0].technologies);
   return (
     <div className="projects-individual-design">
       {design.map((designProject) => (
         <div className="projects-individual-container" key={designProject.id}>
-          <div className="projects-individual-image-div">
-            <img src="{designProject.image}" alt="project visual"></img> 
-          </div>
           <div className="projects-individual-header-div">
-            <h3 className="projects-individual-header-body">{designProject.title}</h3>
+            <h3 className="projects-individual-header-body">
+              {designProject.title}
+            </h3>
+          </div>
+          <div className="projects-individual-image-div">
+            <img className="projects-individual-image-body" src={designProject.image} alt={designProject.title}></img>
           </div>
           <div className="projects-individual-summary-div">
-            <p className="projects-individual-summary-body"> {designProject.summary}</p>
+            <p className="projects-individual-summary-body">
+              {designProject.summary}
+            </p>
           </div>
-          <div className="projects-individual-icons-div">
-            <p className="projects-individual-icons-body">{designProject.technologies}</p>
+          <div className="projects-individual-tech-div">
+            {designProject.technologies.map((tech) => (
+              <>
+                <p className="projects-individual-tech-body">{tech}</p>
+                <span className="projects-individual-tech-body-span">·</span>
+              </>
+            ))}
           </div>
           <div className="projects-individual-actions-div">
-            <p className="projects-individual-actions-body">{designProject.links}</p>
+            {/* {designProject.links.map(link => (
+            <p className="projects-individual-actions-body">{link}</p>
+          ))} */}
+            <GitHubIcon />
+            <OpenInNewIcon />
           </div>
         </div>
       ))}
