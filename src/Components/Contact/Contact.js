54e4c91f3b47ee52779c094d51399d49d818a595
leoarthurwall <leowall93@gmail.com>
2022-08-07 12:24:35 +0100

    added footer section. gave icons path links. made responsive for tablet, mobile and desktop throughout.nav bar logo now responsive with screen size. font sizes updated for tablet. all sections have padding for tablet and desktop.

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,11 @@
 import "../Contact/contact.scss";
 import LinkButton from "../LinkButton/LinkButton";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import resume from '../../Images/resume.png'
+
+
 
 function Contact() {
   return (
@@ -20,6 +26,23 @@ function Contact() {
           <em>feel free to reach out</em>.{" "}
         </h3>
         <LinkButton hrefProp={`mailto:leowall93@gmail.com`} bodyProp={"Contact"}/>
+        <div className="contact-footer">
+          <h5 className="contact-footer-made-by">Designed & Built by Leo Wall</h5>
+          <div className="contact-footer-icon-container">
+            <a className="contact-footer-icon-individual" href={"https://github.com/leoarthurwall"} target="_blank" rel="noopener noreferrer">
+              <GitHubIcon sx={{fontSize: "1.2rem" }}/>  
+            </a>
+            <a className="contact-footer-icon-individual" href={"https://www.linkedin.com/in/leo-wall/"} target="_blank" rel="noopener noreferrer">
+              <LinkedInIcon sx={{fontSize: "1.2rem" }}/>
+            </a>
+            <a className="contact-footer-icon-individual" href={`mailto:leowall93@gmail.com`} target="_blank" rel="noopener noreferrer">
+              <EmailIcon sx={{fontSize: "1.2rem" }}/>
+            </a>
+            <a className="contact-footer-icon-individual" href={`/resume.pdf`} target="_blank" rel="noopener noreferrer">
+              <img className="contact-footer-resume-icon" src={resume} alt= "view resume" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
