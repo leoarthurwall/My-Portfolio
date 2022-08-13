3a750ea9a665b8a33929e449155b8b13f3e1159f
leoarthurwall <leowall93@gmail.com>
2022-08-13 12:42:13 +0100

    improved the about content further. Added the tech section to the right column

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -18,33 +18,40 @@ function About() {
                 My journey into the world of tech began at the start of 2022.
                 Prior to this, I was Playing national level rugby across Spain
                 for FC Barcelona Rugby club. It was breaking my leg during a
-                rugby match last year that enabled me to get perspective on how
-                I wanted my future look.{" "}
+                rugby match last year that enabled me to get perspective and
+                take action on how I wanted my future to look.{" "}
               </p>
               <p className="about-section-summary-p">
-                With my heart set on building a career in tech, I moved back to England
-                to enrol on a 12-week creative computing bootcamp at Bath Spa
-                University where I developed the skills and experience with all
-                things UX/UI.{" "}
+                Having lived in Barcelona for 6 years, I was already under the
+                influence of its booming tech industry, and eager for a way in.
+                So, I decided it was a good time to move back to England and
+                develop the skills and experience for a career in tech.{" "}
+              </p>{" "}
+              <p className="about-section-summary-p">
+                January this year, I enrolled on a 12-week creative computing
+                bootcamp at Bath Spa University. Here, I developed industry
+                skills related to User Interface (UI)/ User Experience (UX)
+                design, product/ project management, and public speaking.{" "}
               </p>
               <p className="about-section-summary-p">
-                With a hunger and curiosity to experience software development
-                too, I then enrolled on the School Of Code 16-week full-stack
-                development bootcamp. I had the privilege of collaborating with
-                amazing people, learning many new technologies and building
-                full-stack applications.{" "}
+                Curious for a deeper dive into software development, in April I
+                enrolled on the School Of Code 16-week full-stack development
+                bootcamp. I had the privilege of collaborating with amazing
+                people, learning many new technologies and building full-stack
+                applications.{" "}
               </p>
               <p className="about-section-summary-p">
                 If I’m not designing and developing applications, you’ll likely
-                find me doing what else I love: road cycling, playing rugby,
+                find me doing what I also love: road cycling, playing rugby,
                 sailing, surfing, calisthenics, travelling, playing the piano,
                 or having a tinker in the garage.{" "}
               </p>
-              <AboutTechs />
+              
             </text>
           </div>
           <div className="about-section-right">
             <img className="about-section-profile-photo" src={pngegg} alt="" />
+            <AboutTechs />
           </div>
         </div>
       </div>
