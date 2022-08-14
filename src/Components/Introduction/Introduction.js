31f32a080b7a8f51b88c6f0b26949a9e0d667254
leoarthurwall <leowall93@gmail.com>
2022-08-14 17:55:54 +0100

    added - to full-stack in the intro

--- src/Components/Introduction/Introduction.js
+++ src/Components/Introduction/Introduction.js
@@ -10,7 +10,7 @@ function Introduction() {
               <p className='intro-main-text-hi'>Hi<em>,</em> my name is</p>
               <h1 className='intro-main-text-name'>Leo Wall.</h1>
               <h2 className='intro-main-text-role'>I design & develop things<em>.</em></h2>
-              <p className='intro-main-text-summary'> I'm a <em>UX/UI designer</em> & <em>full stack developer</em>. I love solving problems, expressing my creative side, and collaborating with people.</p>
+              <p className='intro-main-text-summary'> I'm a <em>UX/UI designer</em> & <em>full-stack developer</em>. I love solving problems, expressing my creative side, and collaborating with people.</p>
               <LinkButton hrefProp={`mailto:leowall93@gmail.com`} bodyProp={"Contact"}/>
             </div>
        
