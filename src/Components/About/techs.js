bf8375ae601a52829372ff84a2bbba8925ceee1d
leoarthurwall <leowall93@gmail.com>
2022-10-10 15:44:33 +0200

    reordered the size of the about technologies

--- src/Components/About/techs.js
+++ src/Components/About/techs.js
@@ -1,107 +1,107 @@
 import "../About/about.scss";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
-function AboutTechs(){
-    return(
-        <div className="about-section-tech-section">
-                <h4 className="about-section-summary-h4">
-                  Here are some of the technologies I've worked with:
-                </h4>
-              
+function AboutTechs() {
+  return (
+    <div className="about-section-tech-section">
+      <h4 className="about-section-summary-h4">
+        Here are some of the technologies I've worked with:
+      </h4>
 
-              <div className="about-section-tech-container">
-                <div className="about-section-tech-body">
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">HTML 5</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">CSS 3</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Javascript (ES6)</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Jest</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Git/GitHub</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Netlify</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">SCSS</li>
-                  </div>
-                </div>
-                <div className="about-section-tech-body">
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">React js</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Next js</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Node js</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Cypress</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Heroku</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">PostgresSQL</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Styled Components</li>
-                  </div>
-                </div>
-                <div className="about-section-tech-body">
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">AWS</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon  sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Figma</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Miro</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Firebase</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Trello</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Auth0</li>
-                  </div>
-                  <div className="about-section-tech-arrow-list-container">
-                    <ArrowRightIcon sx={{color: "#DD6031" }} fontSize="small"/>
-                    <li className="about-section-tech-list">Typescript</li>
-                  </div>
-                </div>
-              </div>
-              </div>
-    )
+      <div className="about-section-tech-container">
+        <div className="about-section-tech-body">
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Figma</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">HTML</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">CSS</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Javascript</li>
+          </div>
+        </div>
+        <div className="about-section-tech-body">
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">React js</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Typescript</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Cypress</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Jest</li>
+          </div>
+        </div>
+        <div className="about-section-tech-body">
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Node.js</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Express.js</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">PostgresSQL</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Firebase</li>
+          </div>
+
+          {/* <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">AWS</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Miro</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Next js</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Auth0</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Heroku</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Git/GitHub</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Netlify</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">SCSS</li>
+          </div>
+          <div className="about-section-tech-arrow-list-container">
+            <ArrowRightIcon sx={{ color: "#DD6031" }} fontSize="small" />
+            <li className="about-section-tech-list">Styled Components</li>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
 }
-export default AboutTechs
+export default AboutTechs;
