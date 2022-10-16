b6cbd826392e473a438088c55b74b1201f50a444
leoarthurwall <leowall93@gmail.com>
2022-10-16 23:18:15 +0100

    gived basic styling to the menu bar. The styling needs work, however, it is functioning as desired

--- src/Components/Navbar/NavMobile/NavMobile.js
+++ src/Components/Navbar/NavMobile/NavMobile.js
@@ -3,10 +3,9 @@ import { Link } from "react-scroll";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import EmailIcon from '@mui/icons-material/Email';
-import ContactPageIcon from '@mui/icons-material/ContactPage';
-
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import ContactPageIcon from "@mui/icons-material/ContactPage";
 
 const NavMobile = () => {
   const [menuClicked, setMenuClicked] = useState(false);
@@ -70,20 +69,52 @@ const NavMobile = () => {
                 Contact
               </Link>
             </li>
-            <div className="contact-footer-icon-container">
-            <a className="contact-footer-icon-individual" href={"https://github.com/leoarthurwall"} target="_blank" rel="noopener noreferrer">
-              <GitHubIcon sx={{fontSize: "1.2rem" }}/>  
-            </a>
-            <a className="contact-footer-icon-individual" href={"https://www.linkedin.com/in/leo-wall/"} target="_blank" rel="noopener noreferrer">
-              <LinkedInIcon  sx={{fontSize: "1.2rem" }}/>
-            </a>
-            <a className="contact-footer-icon-individual" href={`mailto:leowall93@gmail.com`} target="_blank" rel="noopener noreferrer">
-              <EmailIcon  sx={{fontSize: "1.2rem" }}/>
-            </a>
-            <a className="contact-footer-icon-individual" href={`Resume/ResumeLeoWall.pdf`} target="_blank" rel="noopener noreferrer">
-            <ContactPageIcon  sx={{fontSize: "1.2rem" }}/>
-            </a>
-          </div>
+            <div className="modal-icon-container">
+              <div className="nav-mobile-modal-icon-container">
+                <a
+                  className="modal-icon-individual"
+                  href={"https://github.com/leoarthurwall"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <GitHubIcon sx={{ fontSize: "1.2rem" }} />
+                  <p>GitHub</p>
+                </a>
+              </div>
+              <div className="nav-mobile-modal-icon-container">
+                <a
+                  className="modal-icon-individual"
+                  href={"https://www.linkedin.com/in/leo-wall/"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <LinkedInIcon sx={{ fontSize: "1.2rem" }} />
+                  <p>Linkedin</p>
+                </a>
+              </div>
+              <div className="nav-mobile-modal-icon-container">
+                <a
+                  className="modal-icon-individual"
+                  href={`mailto:leowall93@gmail.com`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <EmailIcon sx={{ fontSize: "1.2rem" }} />
+                  <p>Email</p>
+                </a>
+              </div>
+              <div className="nav-mobile-modal-icon-container">
+                <a
+                  className="modal-icon-individual"
+                  href={`Resume/ResumeLeoWall.pdf`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <ContactPageIcon sx={{ fontSize: "1.2rem" }} />
+                  <p>CV</p>
+                </a>
+              </div>
+            </div>
           </ul>
         </div>
       ) : null}
