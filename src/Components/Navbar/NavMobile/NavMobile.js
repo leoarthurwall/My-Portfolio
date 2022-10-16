6f0398928159fa28b5cd38f7494561ce3149db00
leoarthurwall <leowall93@gmail.com>
2022-10-16 22:53:07 +0100

    added section links to modal

--- src/Components/Navbar/NavMobile/NavMobile.js
+++ src/Components/Navbar/NavMobile/NavMobile.js
@@ -1,4 +1,6 @@
 import "../navbar.scss";
+import { Link } from "react-scroll";
+import LinkButton from "../../LinkButton/LinkButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 
@@ -17,7 +19,55 @@ const NavMobile = () => {
         <MenuIcon sx={{ fontSize: "2.5rem" }} onClick={handleMenuClick}/>
       </div>
       {menuClicked ? ( <div className="nav-menu-modal-container">
-        
+      <ul className="nav-list-container-mobile">
+        <li className="nav-list-item-mobile">
+          <Link
+            className="nav-link-mobile"
+            activeClass="active"
+            to="about-section-header"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            About
+          </Link>
+        </li>
+
+        <li className="nav-list-item-mobile">
+          <Link
+            className="nav-link-mobile"
+            activeClass="active"
+            to="projects-section-header"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Projects
+          </Link>
+        </li>
+        <li className="nav-list-item-mobile">
+          <Link
+            className="nav-link-mobile"
+            activeClass="active"
+            to="contact-header"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Contact
+          </Link>
+        </li>
+        <li className="nav-list-item-mobile">
+          <LinkButton
+            className="nav-link-mobile"
+            hrefProp={`Resume/ResumeLeoWall.pdf`}
+            bodyProp={"CV"}
+          />
+        </li>
+      </ul>
       </div>):(null)}
      
     </nav>
