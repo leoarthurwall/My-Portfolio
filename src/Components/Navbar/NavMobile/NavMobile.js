aceb8e5836d72c6baad667bdab08d0059bb1b4fc
leoarthurwall <leowall93@gmail.com>
2022-10-16 22:56:27 +0100

    added a handle click so that when the link button is pressed on the  modal, the state is set to false and the modal closes so the section can be viewed

--- src/Components/Navbar/NavMobile/NavMobile.js
+++ src/Components/Navbar/NavMobile/NavMobile.js
@@ -7,69 +7,75 @@ import { useState } from "react";
 const NavMobile = () => {
   const [menuClicked, setMenuClicked] = useState(false);
 
-    const handleMenuClick = () => {
-        setMenuClicked(!menuClicked)
-    }
+  const handleMenuClick = () => {
+    setMenuClicked(!menuClicked);
+  };
+
+  const handleLinkClick = () => {
+    setMenuClicked(false);
+  };
 
   return (
-
-
     <nav className="nav-mobile-container">
-      <div className="nav-menu-icon-container" >
-        <MenuIcon sx={{ fontSize: "2.5rem" }} onClick={handleMenuClick}/>
+      <div className="nav-menu-icon-container">
+        <MenuIcon sx={{ fontSize: "2.5rem" }} onClick={handleMenuClick} />
       </div>
-      {menuClicked ? ( <div className="nav-menu-modal-container">
-      <ul className="nav-list-container-mobile">
-        <li className="nav-list-item-mobile">
-          <Link
-            className="nav-link-mobile"
-            activeClass="active"
-            to="about-section-header"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-          >
-            About
-          </Link>
-        </li>
+      {menuClicked ? (
+        <div className="nav-menu-modal-container">
+          <ul className="nav-list-container-mobile">
+            <li className="nav-list-item-mobile">
+              <Link
+                onClick={handleLinkClick}
+                className="nav-link-mobile"
+                activeClass="active"
+                to="about-section-header"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                About
+              </Link>
+            </li>
 
-        <li className="nav-list-item-mobile">
-          <Link
-            className="nav-link-mobile"
-            activeClass="active"
-            to="projects-section-header"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-          >
-            Projects
-          </Link>
-        </li>
-        <li className="nav-list-item-mobile">
-          <Link
-            className="nav-link-mobile"
-            activeClass="active"
-            to="contact-header"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-          >
-            Contact
-          </Link>
-        </li>
-        <li className="nav-list-item-mobile">
-          <LinkButton
-            className="nav-link-mobile"
-            hrefProp={`Resume/ResumeLeoWall.pdf`}
-            bodyProp={"CV"}
-          />
-        </li>
-      </ul>
-      </div>):(null)}
-     
+            <li className="nav-list-item-mobile">
+              <Link
+                onClick={handleLinkClick}
+                className="nav-link-mobile"
+                activeClass="active"
+                to="projects-section-header"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="nav-list-item-mobile">
+              <Link
+                onClick={handleLinkClick}
+                className="nav-link-mobile"
+                activeClass="active"
+                to="contact-header"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                Contact
+              </Link>
+            </li>
+            <li className="nav-list-item-mobile">
+              <LinkButton
+                className="nav-link-mobile"
+                hrefProp={`Resume/ResumeLeoWall.pdf`}
+                bodyProp={"CV"}
+              />
+            </li>
+          </ul>
+        </div>
+      ) : null}
     </nav>
   );
 };
