f3b428e0918c81571c5f18d2f7323c402bf990cc
leoarthurwall <leowall93@gmail.com>
2022-10-16 21:43:24 +0100

    imported menu icon & usestate to the navmobile

--- src/Components/Navbar/NavMobile/NavMobile.js
+++ src/Components/Navbar/NavMobile/NavMobile.js
@@ -1,9 +1,12 @@
 import "../navbar.scss";
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
 
 const NavMobile = () => {
+    const [menuClicked, setMenuClicked] = useState(false)
   return (
     <nav>
-    
+        <MenuIcon />
     </nav>
   )
 }
