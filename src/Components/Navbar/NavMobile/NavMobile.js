cfba73b576a365369b2cc2fb857bbd58a45a8f0d
leoarthurwall <leowall93@gmail.com>
2022-10-16 23:05:27 +0100

    added email, github, protfolio and CV icons with links to the modal. need to add text and style

--- src/Components/Navbar/NavMobile/NavMobile.js
+++ src/Components/Navbar/NavMobile/NavMobile.js
@@ -1,8 +1,12 @@
 import "../navbar.scss";
 import { Link } from "react-scroll";
-import LinkButton from "../../LinkButton/LinkButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import ContactPageIcon from '@mui/icons-material/ContactPage';
+
 
 const NavMobile = () => {
   const [menuClicked, setMenuClicked] = useState(false);
@@ -66,13 +70,20 @@ const NavMobile = () => {
                 Contact
               </Link>
             </li>
-            <li className="nav-list-item-mobile">
-              <LinkButton
-                className="nav-link-mobile"
-                hrefProp={`Resume/ResumeLeoWall.pdf`}
-                bodyProp={"CV"}
-              />
-            </li>
+            <div className="contact-footer-icon-container">
+            <a className="contact-footer-icon-individual" href={"https://github.com/leoarthurwall"} target="_blank" rel="noopener noreferrer">
+              <GitHubIcon sx={{fontSize: "1.2rem" }}/>  
+            </a>
+            <a className="contact-footer-icon-individual" href={"https://www.linkedin.com/in/leo-wall/"} target="_blank" rel="noopener noreferrer">
+              <LinkedInIcon  sx={{fontSize: "1.2rem" }}/>
+            </a>
+            <a className="contact-footer-icon-individual" href={`mailto:leowall93@gmail.com`} target="_blank" rel="noopener noreferrer">
+              <EmailIcon  sx={{fontSize: "1.2rem" }}/>
+            </a>
+            <a className="contact-footer-icon-individual" href={`Resume/ResumeLeoWall.pdf`} target="_blank" rel="noopener noreferrer">
+            <ContactPageIcon  sx={{fontSize: "1.2rem" }}/>
+            </a>
+          </div>
           </ul>
         </div>
       ) : null}
