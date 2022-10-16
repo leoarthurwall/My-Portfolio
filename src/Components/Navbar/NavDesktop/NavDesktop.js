aad6ae2de92df4c8aa7ae796218e63ab01ebb3a6
leoarthurwall <leowall93@gmail.com>
2022-10-16 21:40:26 +0100

    created a navmobile component where the burgermenu will be rendered

--- src/Components/Navbar/NavDesktop/NavDesktop.js
+++ src/Components/Navbar/NavDesktop/NavDesktop.js
@@ -5,57 +5,57 @@ import "../navbar.scss";
 const NavDesktop = () => {
   return (
     <nav>
-    <ul className="nav-list-container">
-      <li className="nav-list-item">
-        <Link
-          className="nav-link"
-          activeClass="active"
-          to="about-section-header"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >
-          About
-        </Link>
-      </li>
+      <ul className="nav-list-container">
+        <li className="nav-list-item">
+          <Link
+            className="nav-link"
+            activeClass="active"
+            to="about-section-header"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            About
+          </Link>
+        </li>
 
-      <li className="nav-list-item">
-        <Link
-          className="nav-link"
-          activeClass="active"
-          to="projects-section-header"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >
-          Projects
-        </Link>
-      </li>
-      <li className="nav-list-item">
-        <Link
-          className="nav-link"
-          activeClass="active"
-          to="contact-header"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >
-          Contact
-        </Link>
-      </li>
-      <li className="nav-list-item">
-        <LinkButton
-          className="nav-link"
-          hrefProp={`Resume/ResumeLeoWall.pdf`}
-          bodyProp={"CV"}
-        />
-      </li>
-    </ul>
-  </nav>
-  )
-}
+        <li className="nav-list-item">
+          <Link
+            className="nav-link"
+            activeClass="active"
+            to="projects-section-header"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Projects
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link
+            className="nav-link"
+            activeClass="active"
+            to="contact-header"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Contact
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <LinkButton
+            className="nav-link"
+            hrefProp={`Resume/ResumeLeoWall.pdf`}
+            bodyProp={"CV"}
+          />
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default NavDesktop
+export default NavDesktop;
---
+++ src/Components/Navbar/NavMobile/NavMobile.js
@@ -0,0 +1,11 @@
+import "../navbar.scss";
+
+const NavMobile = () => {
+  return (
+    <nav>
+    
+    </nav>
+  )
+}
+
+export default NavMobile
