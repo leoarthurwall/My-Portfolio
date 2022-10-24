6fabc689957b73fada68cb56750e6da2937e0f41
leoarthurwall <leowall93@gmail.com>
2022-10-24 19:11:56 +0100

    noticed project header was not aligned with project main section. wrapped header inside main and removes align items center so the text can sit on left

--- src/Components/Projects/ProjectsSection.js
+++ src/Components/Projects/ProjectsSection.js
@@ -4,18 +4,23 @@ import ProjectIndividual from "./ProjectIndividual";
 function ProjectsSection() {
   return (
     <div className="projects-section-container">
-      <div className="projects-section-header">
-        <h1 className="projects-header-wording">Projects</h1>
-        <hr className="projects-header-wording-line"></hr>
-      </div>
-      <a className="project-summary-text-a" href="https://github.com/leoarthurwall" target="_blank" rel="noopener noreferrer">
-        <p className="project-summary-text">
-          Here is a selection of my work. If you're interested in seeing more of
-          my work, feel free to head over to my{" "}
-          <span className="word-highlight">GitHub</span> profile.
-        </p>
-      </a>
       <div className="projects-section-main">
+        <div className="projects-section-header">
+          <h1 className="projects-header-wording">Projects</h1>
+          <hr className="projects-header-wording-line"></hr>
+        </div>
+        <a
+          className="project-summary-text-a"
+          href="https://github.com/leoarthurwall"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p className="project-summary-text">
+            Here is a selection of my work. If you're interested in seeing more
+            of my work, feel free to head over to my{" "}
+            <span className="word-highlight">GitHub</span> profile.
+          </p>
+        </a>
         <ProjectIndividual />
       </div>
     </div>
