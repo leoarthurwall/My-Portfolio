57c79772b9e9981924c1830f2e5db5ad38062fa6
leoarthurwall <leowall93@gmail.com>
2023-01-08 22:29:56 +0100

    updated the infro to state frontend dev

--- src/Components/Introduction/Introduction.js
+++ src/Components/Introduction/Introduction.js
@@ -9,8 +9,8 @@ function Introduction() {
             <div className='intro-main-text'>
               <p className='intro-main-text-hi'>Hi<em>,</em> my name is</p>
               <h1 className='intro-main-text-name'>Leo Wall.</h1>
-              <h2 className='intro-main-text-role'>I design & develop things<em>.</em></h2>
-              <p className='intro-main-text-summary'> I'm a <em>UX/UI designer</em> & <em>full-stack developer</em>. I love solving problems, expressing my creative side, and collaborating with people.</p>
+              <h2 className='intro-main-text-role'>Front End developer</h2>
+              <p className='intro-main-text-summary'> with a passion for building responsive applications that <em>solve</em> real-world user <em>problems</em>.</p>
               <LinkButton hrefProp={`mailto:leowall93@gmail.com`} bodyProp={"Contact"}/>
             </div>
        
