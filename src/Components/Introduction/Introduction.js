3b6429cc23f7830b9400851078cdb4d4aae55c3b
leoarthurwall <leowall93@gmail.com>
2023-01-08 22:45:49 +0100

    removed the hi my name is from the intro

--- src/Components/Introduction/Introduction.js
+++ src/Components/Introduction/Introduction.js
@@ -7,8 +7,7 @@ function Introduction() {
       <div main className='intro-container'>
           
             <div className='intro-main-text'>
-              <p className='intro-main-text-hi'>Hi<em>,</em> my name is</p>
-              <h1 className='intro-main-text-name'>Leo Wall.</h1>
+              <h1 className='intro-main-text-name'>Leo Wall<em>.</em></h1>
               <h2 className='intro-main-text-role'>Front End developer</h2>
               <p className='intro-main-text-summary'> with a passion for building responsive applications that <em>solve</em> real-world user <em>problems</em>.</p>
               <LinkButton hrefProp={`mailto:leowall93@gmail.com`} bodyProp={"Contact"}/>
